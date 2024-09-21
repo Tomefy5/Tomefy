47d57ee2a8145b838e6eafb9648e1e8ffd4454cd
tomefy andry tsiresy <tomefyandrytsiresy@gmail.com>
2024-09-21 11:38:53 +0300

    Content redaction

--- src/Components/ProfilDescri.js
+++ src/Components/ProfilDescri.js
@@ -5,11 +5,10 @@ import Profil from "./Profil";
 function ProfilDescri() {
     return (
         <div className="container-fluid mt-5 mb-5">
-            <Profil/>
+            <Profil />
             <div className="container-fluid p-2 mt-3">
-                <h1 className="profil-competence">Web Developer</h1>
-                <p className="competence-descri">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum aperiam, eveniet veritatis labore facilis minus 
-                    non magni et voluptate odit impedit? Rem odio reprehenderit quod officia sapiente molestias, voluptatibus sed </p>
+                <h1 className="profil-competence">Versatile Developer</h1>
+                <p className="competence-descri">As a versatile developer, I see myself as the Swiss Army knife of IT—handy in any situation! Whether diving into back-end mysteries or crafting sleek front-end experiences, I’m eager to explore the tech world. On a quest for knowledge, I turn caffeine into code and tackle challenges with a smile. Let’s create something amazing together! </p>
             </div>
         </div>
     )
